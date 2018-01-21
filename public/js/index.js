55a2612e6777e0e44dfb0468c9c7971fa7954d8e
abhishekmahto88 <abhishekmahto88@outlook.com>
2018-01-21 12:02:13 +0530

    Scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,24 @@
 var socket = io();
 
+function scrollToBottom(){
+  //Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+    console.log('Should scroll');
+  }
+}
+
 socket.on('connect',function(){
  console.log('Connected to server.');
-
- // socket.emit('createEmail',{
- //   to:'shrimankumbar@gmail.com',
- //   text:'Hey,this is Shriman.'
- // });
-
-
 });
 
 socket.emit('newMessage',{
@@ -21,11 +31,7 @@ socket.emit('newMessage',{
 
 socket.on('disconnect',function(){
  console.log('Disconnected from server.');
-})
-
-// socket.on('newEmail',function(email){
-//   console.log('New email',email);
-// });
+});
 
 socket.on('newMessage',function(message){
   var template = jQuery('#message-template').html();
@@ -36,29 +42,19 @@ socket.on('newMessage',function(message){
     createdAt: formattedTime
   });
   jQuery('#messages').append(html);
- // console.log('New Message',message);
- // var formattedTime = moment(message.createdAt).format('h:mm a');
- // var li =jQuery('<li></li>');
- // li.text(`${message.from} ${formattedTime} : ${message.text}`);
- // jQuery("#messages").append(li);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage',function (message) {
   var template = jQuery('#location-message-template').html();
-  var formattedTime = moment().format('h:mm a');
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   var html = Mustache.render(template,{
     url: message.url,
     from: message.from,
     createdAt: formattedTime
   });
   jQuery('#messages').append(html);
-  // var formattedTime = moment().format('h:mm a');
-  // var li= jQuery('<li></li>');
-  // var a = jQuery('<a target="_blank">My current location</a>');
-  // li.text(`${message.from} ${formattedTime}: `);
-  // a.attr('href',message.url);
-  // li.append(a);
-  // jQuery('#messages').append(li);
+  scrollToBottom();
 });
 
 jQuery("#message-form").on('submit',function(e){
@@ -91,66 +87,3 @@ locationButton.on('click',function () {
     alert('Unable to fetch location.');
   })
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var socket = io();
-// socket.on('connect',function (){
-//   console.log('connected to server.');
-//
-// socket.on('disconnect',function (){
-//   console.log('Dissconnected from server.');
-// });
-//
-// socket.on('newEmail',function(email){
-//   console.log('New email',email);
-// });
-//
-// socket.on('newMessage',function(message){
-//   console.log('newMessage',message);
-//   var li = jQuery('<li></li>');
-//   li.text(`${message.from}: ${message.text}`);
-//   jQuery('#messges').append(li);
-// });
-//
-// socket.emit('createMessage',{
-//   from: 'Frank',
-//   text: 'Hi'
-// },function(data) {
-//   console.log('Got it',data);
-// });
-//
-// jQuery('#message-form').on('submit',function (e) {
-//   e.preventDefault();
-//   socket.emit('createMessage',{
-//     from: 'User',
-//     text: jQuery('[name=message]').val();
-//   },function() {
-//
-//   });
-// });
-//
-// // socket.emit('createMessage',{
-// //    to:"Prashanth",
-// //    from:"Faizan",
-// //    text:"Hey, Whats up.",
-// //    createAt:new Date()
-// //  },function(data) {
-// //    console.log("got it ", data);
-// //  });
