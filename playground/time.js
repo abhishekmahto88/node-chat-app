875a6dcd9d27058e5185077b727c8b854effef05
abhishekmahto88 <abhishekmahto88@outlook.com>
2018-01-21 11:00:12 +0530

    Timestamp

--- playground/time.js
+++ playground/time.js
@@ -1,4 +1,28 @@
-// Jan 1st 1970 00:00:00 am
-var date=new Date();
-var months =['Jan','Feb'];
-console.log(date.getMonth());
+const moment = require('moment');
+var someTimestamp = moment()
+var createdAt=1234;
+var date = moment(createdAt);
+console.log(date.format('h:mm a'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var date = moment();
+// // date.add(100,'year').subtract(9,'months');
+// console.log(date.format('h:mm a'));
+// // Jan 1st 1970 00:00:00 am
+// // var date=new Date();
+// // var months =['Jan','Feb'];
+// // console.log(date.getMonth());
